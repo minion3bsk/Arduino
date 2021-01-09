efed85c4b4ae6f479fe8705e79d700b8b9814005
長谷川真緒 <chocolate@Mao-MacBook-Pro.local>
2021-01-09 12:07:37 +0900

    remove something

--- dance_robot_1/dance_robot_1.ino
+++ dance_robot_1/dance_robot_1.ino
@@ -1,15 +1,15 @@
 
 
 void setup(){
-  pinMode(3,OUTPUT);
-  pinMode(11,OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(9, OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 void loop(){
-  digitalWrite(3,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(9,LOW);
   digitalWrite(11,LOW);
-  digitalWrite(6,LOW);
-  digitalWrite(9,HIGH);
-}
+  digitalWrite(12,HIGH);
+}c
